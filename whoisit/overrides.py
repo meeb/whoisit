d33246d5c5f87c94a960b272b2a92b024dbdaf7e
case-fastly <141883771+case-fastly@users.noreply.github.com>
2023-12-13 13:57:08 -0800

    Add Identity Digital-hosted ccTLD RDAP URLs

--- whoisit/overrides.py
+++ whoisit/overrides.py
@@ -29,6 +29,18 @@ iana_overrides = {
         # 2023-09-05 - .nl has an RDAP endpoint it's just not listed
         'nl': ['https://rdap.sidn.nl/'],
 
+        # 2023-12-13 - the Identity Digital RDAP server appears to support these ccTLDs
+        'ac': ['https://rdap.identitydigital.services/'],
+        'ag': ['https://rdap.identitydigital.services/'],
+        'bz': ['https://rdap.identitydigital.services/'],
+        'io': ['https://rdap.identitydigital.services/'],
+        'lc': ['https://rdap.identitydigital.services/'],
+        'me': ['https://rdap.identitydigital.services/'],
+        'mn': ['https://rdap.identitydigital.services/'],
+        'pr': ['https://rdap.identitydigital.services/'],
+        'sc': ['https://rdap.identitydigital.services/'],
+        'sh': ['https://rdap.identitydigital.services/'],
+        'vc': ['https://rdap.identitydigital.services/'],
     }
 
 }

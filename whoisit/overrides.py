c82d54eda6061c9cfa376a13efc24b50d891134e
Eric Case <case@users.noreply.github.com>
2025-12-07 12:54:01 -0800

    Add override RDAP server for .us

--- whoisit/overrides.py
+++ whoisit/overrides.py
@@ -8,12 +8,16 @@ iana_overrides: dict[str, ...] = {
     "domain": {
         # 2021-11-30 - .de has an RDAP endpoint it's just not listed
         "de": ["https://rdap.denic.de/"],
+        
         # 2022-01-15 - .ch has an RDAP endpoint it's just not listed
         "ch": ["https://rdap.nic.ch/"],
+        
         # 2023-08-21 - .li has an RDAP endpoint it's just not listed
         "li": ["https://rdap.nic.li/"],
+        
         # 2022-10-17 - .gl and .gl SLDs have RDAP endpoints they're just not listed
         "gl": ["https://rdap.centralnic.com/gl/"],
+        
         # 2024-04-30 - the Identity Digital RDAP server appears to support these ccTLDs
         "ac": ["https://rdap.identitydigital.services/rdap/"],
         "ag": ["https://rdap.identitydigital.services/rdap/"],
@@ -26,7 +30,11 @@ iana_overrides: dict[str, ...] = {
         "sc": ["https://rdap.identitydigital.services/rdap/"],
         "sh": ["https://rdap.identitydigital.services/rdap/"],
         "vc": ["https://rdap.identitydigital.services/rdap/"],
+        
         # 2025-11-10 - .co's stealth (unlisted) RDAP endpoint has moved
         "co": ["https://rdap.registry.co/co/"],
+
+        # 2025-12-07 - GoDaddy Registry operates this
+        "us": ["https://rdap.nic.us/"],
     }
 }

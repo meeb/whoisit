ac08a1dee11d85fe6e945c20f60a7a8647424647
meeb <meeb@meeb.org>
2026-01-05 14:51:37 +1100

    sync overrides

--- whoisit/overrides.py
+++ whoisit/overrides.py
@@ -2,39 +2,69 @@
 Various RDAP servers are either incorrect or not listed in the IANA
 bootstrap data. This file contains a list of overrides that are overlayed
 onto the IANA data by default.
+
+Last updated: 2026-01-05 03:46:00 UTC
 """
+
 
 iana_overrides: dict[str, ...] = {
     "domain": {
-        # 2021-11-30 - .de has an RDAP endpoint it's just not listed
-        "de": ["https://rdap.denic.de/"],
-        
-        # 2022-01-15 - .ch has an RDAP endpoint it's just not listed
-        "ch": ["https://rdap.nic.ch/"],
-        
-        # 2023-08-21 - .li has an RDAP endpoint it's just not listed
-        "li": ["https://rdap.nic.li/"],
-        
-        # 2022-10-17 - .gl and .gl SLDs have RDAP endpoints they're just not listed
-        "gl": ["https://rdap.centralnic.com/gl/"],
-        
-        # 2024-04-30 - the Identity Digital RDAP server appears to support these ccTLDs
-        "ac": ["https://rdap.identitydigital.services/rdap/"],
-        "ag": ["https://rdap.identitydigital.services/rdap/"],
-        "bz": ["https://rdap.identitydigital.services/rdap/"],
-        "io": ["https://rdap.identitydigital.services/rdap/"],
-        "lc": ["https://rdap.identitydigital.services/rdap/"],
-        "me": ["https://rdap.identitydigital.services/rdap/"],
-        "mn": ["https://rdap.identitydigital.services/rdap/"],
-        "pr": ["https://rdap.identitydigital.services/rdap/"],
-        "sc": ["https://rdap.identitydigital.services/rdap/"],
-        "sh": ["https://rdap.identitydigital.services/rdap/"],
-        "vc": ["https://rdap.identitydigital.services/rdap/"],
-        
-        # 2025-11-10 - .co's stealth (unlisted) RDAP endpoint has moved
-        "co": ["https://rdap.registry.co/co/"],
-
-        # 2025-12-07 - GoDaddy Registry operates this
-        "us": ["https://rdap.nic.us/"],
+        "ac": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "ag": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "bh": [
+            "https://rdap.centralnic.com/bh/"
+        ],
+        "bz": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "ch": [
+            "https://rdap.nic.ch/"
+        ],
+        "co": [
+            "https://rdap.registry.co/co/"
+        ],
+        "de": [
+            "https://rdap.denic.de/"
+        ],
+        "gl": [
+            "https://rdap.centralnic.com/gl/"
+        ],
+        "io": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "lc": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "li": [
+            "https://rdap.nic.li/"
+        ],
+        "me": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "mn": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "my": [
+            "https://rdap.mynic.my/rdap/"
+        ],
+        "pr": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "sc": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "sh": [
+            "https://rdap.identitydigital.services/rdap/"
+        ],
+        "us": [
+            "https://rdap.nic.us/"
+        ],
+        "vc": [
+            "https://rdap.identitydigital.services/rdap/"
+        ]
     }
 }
